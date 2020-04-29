4d2da71cefaf68ae981d884fe101cba557c6c8bd
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-04-29 17:01:12 +0100

    testing images

--- src/components/Blog/BlogThumbnail.js
+++ src/components/Blog/BlogThumbnail.js
@@ -5,7 +5,7 @@ function BlogThumbnail({ data }) {
   const { id, frontmatter } = postData
   return (
     <li>
-      <figure style={{ width: "100px" }}>
+      <figure style={{ width: "50px" }}>
         <img src={frontmatter.thumbnailImg} />
       </figure>
       <h2>{frontmatter.title}</h2>
