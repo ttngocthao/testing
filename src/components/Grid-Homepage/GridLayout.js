c5d7e60c4c29d9eaa55f8c28a4f8e708f144e8a4
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-05-19 14:58:15 +0100

    checked gridhome data

--- src/components/Grid-Homepage/GridLayout.js
+++ src/components/Grid-Homepage/GridLayout.js
@@ -3,7 +3,22 @@ import { gridHomeData } from "../../data/dummy.data"
 import GridItem from "./GridItem"
 
 function GridLayout() {
-  //console.log(gridHomeData)
+  console.log(gridHomeData)
+  // const data =useStaticQuery(graphql`
+  // query AllImages {
+  //   allImageSharp {
+  //     edges {
+  //       node {
+  //         fluid {
+  //           originalImg
+  //           originalName
+  //           src
+  //         }
+  //       }
+  //     }
+  //   }
+  // }`)
+
   return (
     <div className="grid-layout">
       {gridHomeData &&
