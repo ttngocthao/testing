4162675ddc0c62bde204e237c20401c7c171d551
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-05-19 20:38:54 +0100

    added Layout for blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,18 @@
 import React from "react"
+import Layout from "../components/Layout/Layout"
 import BlogList from "../components/Blog/BlogList"
+import PageTitle from "../components/PageTiltle/PageTitle"
 
 function Blog() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <Layout>
+      <SEO
+        title="Arthaus Visual Communications - Blog"
+        description="Branding, digital marketing, communications, HSBC"
+      />
+      <PageTitle pageTitle="Tool kit" />
       <BlogList />
-    </div>
+    </Layout>
   )
 }
 
