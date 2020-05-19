69a6243b2823c5f59824611bf23af659c3480ca4
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-05-19 21:16:49 +0100

    added styles for blog

--- src/components/Blog/BlogList.js
+++ src/components/Blog/BlogList.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
+import styles from "./blog.module.scss"
 import BlogThumbnail from "./BlogThumbnail"
+
 function BlogList() {
   const data = useStaticQuery(graphql`
     query BlogListQuery {
@@ -24,55 +26,13 @@ function BlogList() {
   console.log("data blog list", postList)
   // console.log("BlogListQuery", BlogListQuery)
   return (
-    <div>
-      <h1>This is Blog list</h1>
-      <ul>
-        {postList &&
-          postList.map((item, indx) => {
-            return <BlogThumbnail data={item} key={indx} />
-          })}
-      </ul>
-    </div>
+    <ul className={styles.blogList}>
+      {postList &&
+        postList.map((item, indx) => {
+          return <BlogThumbnail data={item} key={indx} />
+        })}
+    </ul>
   )
 }
 
 export default BlogList
-// export default () => {
-//   return (
-//     <StaticQuery
-//       query={graphql`
-//         query BlogQuery {
-//           allMarkdownRemark(sort: { order: ASC }) {
-//             edges {
-//               node {
-//                 excerpt(pruneLength: 20)
-//                 id
-//                 frontmatter {
-//                   path
-//                   title
-//                   date(fromNow: true)
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `}
-//       render={data => <BlogList data={data} />}
-//     />
-//   )
-// }
-// export const BlogListQuery = graphql`
-//   query BlogListQuery {
-//     allMarkdownRemark {
-//       edges {
-//         node {
-//           id
-//           frontmatter {
-//             title
-//             path
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
--- src/components/Blog/BlogThumbnail.js
+++ src/components/Blog/BlogThumbnail.js
@@ -1,30 +1,25 @@
 import React from "react"
-import { Link, graphql, StaticQuery } from "gatsby"
+import { Link } from "gatsby"
 function BlogThumbnail({ data }) {
   const { node: postData } = data
   const { id, frontmatter } = postData
   return (
-    <li>
-      <figure style={{ width: "50px" }}>
-        <img src={frontmatter.thumbnailImg} />
-      </figure>
-      <h2>{frontmatter.title}</h2>
-      <p>{id}</p>
-      <p>{frontmatter.shortIntro}</p>
-      <Link to={frontmatter.path}>Read more</Link>
+    <li className={styles.thumbnailItem} blogid={id}>
+      <ImgCard
+        className={styles.thumbnailImg}
+        src={frontmatter.thumbnailImg}
+        width={220}
+        height={220}
+      />
+
+      <h3 className={styles.thumbnailTitle}>{frontmatter.title}</h3>
+
+      <p className={styles.thumbnailIntro}>{frontmatter.shortIntro}</p>
+      <Link className={styles.thumbnailLink} to={frontmatter.path}>
+        Read more
+      </Link>
     </li>
   )
 }
 
 export default BlogThumbnail
-// export const postQuery = graphql`
-//   query MyQuery {
-//     markdownRemark(frontmatter: {}) {
-//       frontmatter {
-//         date(fromNow: true)
-//         path
-//         title
-//       }
-//     }
-//   }
-// `
