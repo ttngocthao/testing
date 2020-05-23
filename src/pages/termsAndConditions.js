cb691300e927109baf7e7336713bdb52ca111526
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-05-23 17:42:23 +0100

    fixed term and conditions page

--- src/pages/termsAndConditions.js
+++ src/pages/termsAndConditions.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-function termCondition() {
+function termsAndConditions() {
   return <div>term and condition</div>
 }
 
-export default termCondition
+export default termsAndConditions
