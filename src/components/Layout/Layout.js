f83243d96d687055db2f8e711ac7c48dda6ef95d
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-05-26 17:45:54 +0100

    fixed bugs

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,15 +3,14 @@ import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import "../../styles/styles.scss"
 import styles from "./layout.module.scss"
-import { IdentityContextProvider } from "react-netlify-identity"
+
 function Layout({ ...props }) {
-  const url = "https://nostalgic-curran-576267.netlify.app/"
   return (
-    <IdentityContextProvider value={url}>
+    <>
       <Header />
       <main className={styles.main}>{props.children}</main>
       <Footer />
-    </IdentityContextProvider>
+    </>
   )
 }
 
