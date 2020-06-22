e4af05d34043b602286e8290ca8befb93f7ac591
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-06-22 22:11:05 +0100

    added functions

---
+++ functions/sendGrid.js
@@ -0,0 +1,33 @@
+exports.handler = async event => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+      headers: { Allow: "POST" },
+    }
+  }
+  console.log("starting")
+  const data = JSON.parse(event.body)
+  console.log("data", data)
+  const { name } = data
+  const sgMail = require("@sendgrid/mail")
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: "thao.truong@arthaus.co.uk",
+    from: "send grid - netlify function",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: name,
+  }
+  try {
+    await sgMail.send(msg)
+    return {
+      statusCode: 200,
+      body: "Message sent",
+    }
+  } catch (err) {
+    return {
+      statusCode: err.code,
+      body: err.message,
+    }
+  }
+}
---
+++ src/pages/testSG.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Formik, Field, Form, ErrorMessage } from "formik"
+
+const testSG = () => {
+  return (
+    <div>
+      <h1>Test send grid</h1>
+
+      <Formik
+        initialValues={{ name: "" }}
+        onSubmit={(data, { resetForm }) => {
+          fetch("/.netlify/functions/sendGrid", {
+            method: "POST",
+
+            body: data,
+          })
+          console.log("data", data)
+        }}
+      >
+        <Form>
+          <Field name="name" type="text" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  )
+}
+
+export default testSG
