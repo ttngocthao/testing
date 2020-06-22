cd27f44aa731219b581a3ef5cda33fd1e38b64b4
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-06-22 23:28:43 +0100

    updated msg sendGrid

--- functions/sendGrid.js
+++ functions/sendGrid.js
@@ -2,18 +2,19 @@ const sgMail = require("@sendgrid/mail")
 const { SENDGRID_API_KEY } = process.env
 
 exports.handler = async event => {
-  console.log("click", event)
+  console.log("event", event)
   const payload = JSON.parse(event.body)
   console.log("api", SENDGRID_API_KEY)
+  sgMail.setApiKey(SENDGRID_API_KEY)
   const body = Object.keys(payload)
     .map(k => {
       return `${k}: ${payload[k]}`
     })
     .join("<br><br>")
   const msg = {
-    to: SENDGRID_TO_EMAIL,
-    from: email,
-    subject: subject ? subject : "Contact Form Submission",
+    to: "thao.truong@arthaus.co.uk",
+    from: "testing page",
+    subject: "Contact Form Submission",
     html: body,
   }
   console.log("msg", msg)
